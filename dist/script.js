093169ee0f80bce96008e07d144634705c56cfde
ReXerses <xerses95@gmail.com>
2024-07-13 21:40:50 +0200

    1) Creazione di un carosello contenente le    immagini dei vari lavori. 2) Implementazione dei navigation dots per gestire    il cambio di lavoro. 3) Implementazione di buttons per cambiare    facilmente i lavori. 4) Creazione di una riga di info, contenente vari    link come (url repo, url Live) e tecnologia    utilizzata. 5) Creazione di una breve descrizione del lavoro. 6) Caricamento dei media necessari alla section.

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,83 @@
 import { Lavori } from './lavori.js';
-console.log(Lavori);
+const titoloLavori = document.querySelector('.titoloLavoro');
+const homepages = document.querySelector('.homepage');
+const descrizioni = document.querySelector('.descrizione');
+const btnNext = document.getElementById('next');
+const btnPrev = document.getElementById('prev');
+const dotsContainer = document.querySelector('.dots');
+const urlsRepo = document.querySelector('.urlRepo');
+const tecnologie = document.querySelector('.tecnologia');
+const urlsLive = document.querySelector('.urlLive');
+let currentIndex = 0;
+// Creazione dei punti di navigazione
+if (dotsContainer) {
+    for (let i = 0; i < Lavori.length; i++) {
+        const dot = document.createElement('span');
+        dot.classList.add('dot');
+        dot.setAttribute('data-slide', i.toString());
+        if (i === 0) {
+            dot.classList.add('active');
+        }
+        dotsContainer.appendChild(dot);
+    }
+}
+else {
+    console.error('Elemento .dots non trovato');
+}
+const dots = document.querySelectorAll('.dot');
+function updateCarosello(currentIndex) {
+    if (titoloLavori)
+        titoloLavori.textContent = Lavori[currentIndex].titolo;
+    if (homepages)
+        homepages.src = Lavori[currentIndex].img;
+    if (descrizioni)
+        descrizioni.textContent = Lavori[currentIndex].descrizione;
+    if (urlsRepo)
+        urlsRepo.href = Lavori[currentIndex].urlRepo;
+    if (tecnologie)
+        tecnologie.src = Lavori[currentIndex].tec;
+    if (urlsLive)
+        urlsLive.href = Lavori[currentIndex].urlLive;
+    // Aggiorna i punti di navigazione
+    dots.forEach((dot, index) => {
+        if (index === currentIndex) {
+            dot.classList.add("active");
+        }
+        else {
+            dot.classList.remove("active");
+        }
+    });
+}
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % Lavori.length;
+    updateCarosello(currentIndex);
+}
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + Lavori.length) % Lavori.length;
+    updateCarosello(currentIndex);
+}
+/* Event listeners */
+if (btnNext) {
+    btnNext.addEventListener('click', () => {
+        nextSlide();
+    });
+}
+else {
+    console.error('Elemento con ID "next" non trovato o non è un HTMLButtonElement');
+}
+if (btnPrev) {
+    btnPrev.addEventListener('click', () => {
+        prevSlide();
+    });
+}
+else {
+    console.error('Elemento con ID "prev" non trovato o non è un HTMLButtonElement');
+}
+dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+        currentIndex = index;
+        updateCarosello(currentIndex);
+    });
+});
+// primo paint della sezione
+updateCarosello(currentIndex);
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,95 @@
 import {Lavori} from './lavori.js'
 
-console.log(Lavori);
+
+
+const titoloLavori : Element | null = document.querySelector('.titoloLavoro');
+const homepages : HTMLImageElement | null = document.querySelector('.homepage');
+const descrizioni : HTMLSpanElement | null = document.querySelector('.descrizione');
+const btnNext = document.getElementById('next') as HTMLButtonElement | null;
+const btnPrev = document.getElementById('prev') as HTMLButtonElement | null;
+const dotsContainer: HTMLElement | null = document.querySelector('.dots');
+const urlsRepo : HTMLAnchorElement | null = document.querySelector('.urlRepo');
+const tecnologie : HTMLImageElement | null = document.querySelector('.tecnologia');
+const urlsLive : HTMLAnchorElement | null = document.querySelector('.urlLive');
+
+let currentIndex : number = 0;
+
+// Creazione dei punti di navigazione
+if (dotsContainer) {
+    for (let i: number = 0; i < Lavori.length; i++) {
+      const dot: HTMLSpanElement = document.createElement('span');
+      dot.classList.add('dot');
+      dot.setAttribute('data-slide', i.toString());
+  
+      if (i === 0) {
+        dot.classList.add('active');
+      }
+  
+      dotsContainer.appendChild(dot);
+    }
+  } else {
+    console.error('Elemento .dots non trovato');
+  }
+
+const dots: NodeListOf<Element> = document.querySelectorAll('.dot');
+
+
+
+function updateCarosello(currentIndex: number): void {
+
+    if (titoloLavori) titoloLavori.textContent= Lavori[currentIndex].titolo;
+    if (homepages) homepages.src= Lavori[currentIndex].img;
+    if (descrizioni) descrizioni.textContent = Lavori[currentIndex].descrizione;
+    if (urlsRepo) urlsRepo.href = Lavori[currentIndex].urlRepo;
+    if (tecnologie) tecnologie.src = Lavori[currentIndex].tec;
+    if (urlsLive) urlsLive.href = Lavori[currentIndex].urlLive;
+
+    // Aggiorna i punti di navigazione
+    dots.forEach((dot, index) => {
+        if (index === currentIndex) {
+            dot.classList.add("active");
+        } else {
+            dot.classList.remove("active");
+        }
+    });
+}
+
+function nextSlide() : void {
+    currentIndex = (currentIndex + 1) % Lavori.length;
+    updateCarosello( currentIndex);
+}
+
+function prevSlide() : void {
+    currentIndex = (currentIndex - 1 + Lavori.length) % Lavori.length;
+    updateCarosello( currentIndex);
+}
+
+/* Event listeners */
+
+
+if (btnNext) {
+    btnNext.addEventListener('click', () => {
+        nextSlide();
+    });
+} else {
+    console.error('Elemento con ID "next" non trovato o non è un HTMLButtonElement');
+}
+
+if (btnPrev) {
+    btnPrev.addEventListener('click', () => {
+        prevSlide();
+    });
+} else {
+    console.error('Elemento con ID "prev" non trovato o non è un HTMLButtonElement');
+}
+
+dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+        currentIndex = index;
+        updateCarosello(currentIndex);
+    });
+});
+
+// primo paint della sezione
+
+updateCarosello(currentIndex);
