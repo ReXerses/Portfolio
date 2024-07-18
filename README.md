1442f9b5a5bd7a71339494206aee8f84646cf24f
ReXerses <xerses95@gmail.com>
2024-07-18 21:52:24 +0200

    1) Creazione di un menù adatto ai dispositivi     mobile (attivato tramite media query). 2) Aggiunta animazione bounce al menù mobile. 3) Aggiunte animazioni agli elementi delle section     chi sono e lavori, le animazioni si attivano     quando l'utente entra in quella determinata     view port (utilizzo di observer). 4) Eliminazione file inutilizzati. 5) Aggiornamento README.

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Creazione della prima section (Presentazione). <br>
 Creazione della seconda section (Lavori). <br>
 Creazione della terza section (Contatti). <br>
 Creazione pagina successo invio form.  <br>
-Step attuale --- > Ottimizzazioni mobile. <br>
+Ottimizzazioni mobile. <br>
+Aggiunta animazioni. <br>
+Step attuale --- > Ottimizzazioni accessibilità/prestazioni (lighthouse).
--- src/script.ts
+++ src/script.ts
@@ -170,3 +170,79 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+/*  Toggle menu  */
+
+export function toggleMenu(): void {
+    let mobileMenu = document.getElementById('mobileMenu');
+    if (mobileMenu) {
+        mobileMenu.classList.toggle('open');
+        var hamburger = document.querySelector('.hamburger');
+        if (hamburger) {
+            if (mobileMenu.classList.contains('open')) {
+                hamburger.classList.add('svgAttiva');
+            } else {
+                hamburger.classList.remove('svgAttiva');
+            }
+        }
+    }
+}
+
+
+(window as any).toggleMenu = toggleMenu;
+
+/* animazioni quando entrano nel view port */
+
+document.addEventListener('DOMContentLoaded', () => {
+    /* section chi sono */
+    const homeSection: HTMLElement | null = document.getElementById('chiSono');
+    const presentazione: HTMLElement | null = document.querySelector('.presentazione');
+    const fotoCV: HTMLElement | null = document.querySelector('.fotoCV');
+
+    /* section lavori */
+    const lavoriSection: HTMLElement | null = document.getElementById('lavori');
+    const descrizione: HTMLElement | null = document.querySelector('.contenitoreDescrizione');
+    const fotoLavori: HTMLElement | null = document.querySelector('.fotoLavori');
+
+    const observerOptions: IntersectionObserverInit = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    /* Callback per la sezione "chi sono" */
+    const observerCallbackChiSono: IntersectionObserverCallback = (entries, observer) => {
+        entries.forEach(entry => {
+            if (presentazione && fotoCV) {
+                if (entry.isIntersecting) {
+                    presentazione.classList.add('animate-presentazione');
+                    fotoCV.classList.add('animate-firstHalf');
+                } else {
+                    presentazione.classList.remove('animate-presentazione');
+                    fotoCV.classList.remove('animate-firstHalf');
+                }
+            }
+        });
+    };
+
+    /* Callback per la sezione "lavori" */
+    const observerCallbackLavori: IntersectionObserverCallback = (entries, observer) => {
+        entries.forEach(entry => {
+            if (descrizione && fotoLavori) {
+                if (entry.isIntersecting) {
+                    descrizione.classList.add('animate-presentazione');
+                    fotoLavori.classList.add('animate-firstHalf');
+                } else {
+                    descrizione.classList.remove('animate-presentazione');
+                    fotoLavori.classList.remove('animate-firstHalf');
+                }
+            }
+        });
+    };
+
+    const observerChiSono = new IntersectionObserver(observerCallbackChiSono, observerOptions);
+    const observerLavori = new IntersectionObserver(observerCallbackLavori, observerOptions);
+
+    if (observerChiSono && homeSection) observerChiSono.observe(homeSection);
+    if (observerLavori && lavoriSection) observerLavori.observe(lavoriSection);
+});
